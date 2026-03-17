a7acf1501e57723047ad3ab71c524a8fab5b8744
AidenSabbir <rashedulislamsabbir@gmail.com>
2026-03-17 20:18:13 +0600

    Plz

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
+
 export default function Home() {
-  redirect("/inbox");
+  if (window.location.pathname !== '/googlea1061e93d1208ad4.html') { redirect("/inbox"); }
 }
