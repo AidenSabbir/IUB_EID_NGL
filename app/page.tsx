9f2f05c18216b3314551e250a60b9f25c6e42c6a
AidenSabbir <rashedulislamsabbir@gmail.com>
2026-03-17 20:43:33 +0600

    hope2

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    const path = window.location.pathname;
-    if (path !== "/googlea1061e93d1208ad4.html") {
+   if (window.location.pathname === "/") {
       router.push("/inbox");
     }
   }, []);
